2ee815964f729443084b0b4654ae2b71d6aae372
Jonathan McArthur <silverstagecoach@gmail.com>
2022-07-06 15:52:14 -0500

    step 2 commit

---
+++ server/controller.js
@@ -0,0 +1,14 @@
+module.exports = {
+    getHouses: (req, res) =>{
+
+    },
+    deleteHouse: (req, res) => {
+
+    },
+    createHouse: (req, res) => {
+
+    },
+    updateHouse: (req, res) => {
+        
+    }
+}
---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+const controller = require('./controller.js')
+const {getHouses, deleteHouse, createHouse, updateHouse} = controller;
+
+app.get('/api/houses', getHouses);
+app.post('/api/houses', createHouse);
+app.put('/api/houses/:id', updateHouse);
+app.delete('/api/houses/:id', deleteHouse);
+
+app.listen(4004, () => console.log('Server running on 4004'))
